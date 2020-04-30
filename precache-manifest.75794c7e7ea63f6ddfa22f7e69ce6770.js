ed1b22e67a03482e184c5d92ef71a228017bbde6
JW9506 <cf.dota@gmail.com>
2020-04-30 16:14:01 -0400

    Updates

---
+++ precache-manifest.75794c7e7ea63f6ddfa22f7e69ce6770.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763f0191b5329b5b9642f5b658229fc6",
+    "url": "/basic-react-e-commerce-crud/index.html"
+  },
+  {
+    "revision": "568944b4ffa5c5f7d028",
+    "url": "/basic-react-e-commerce-crud/static/css/main.c63718d6.chunk.css"
+  },
+  {
+    "revision": "2bde7fb5b4b07bc208c7",
+    "url": "/basic-react-e-commerce-crud/static/js/2.1fd83532.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/basic-react-e-commerce-crud/static/js/2.1fd83532.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "568944b4ffa5c5f7d028",
+    "url": "/basic-react-e-commerce-crud/static/js/main.a0f676e5.chunk.js"
+  },
+  {
+    "revision": "726930cf0ba7d32a39e7",
+    "url": "/basic-react-e-commerce-crud/static/js/runtime-main.52114e41.js"
+  },
+  {
+    "revision": "37a228bcdeb4477843428039a6573055",
+    "url": "/basic-react-e-commerce-crud/static/media/static_pic.37a228bc.jpg"
+  }
+]);
