231776e64aa7a756156840019379f2a57f212019
JW9506 <cf.dota@gmail.com>
2020-04-30 05:58:13 -0400

    chore: append sass rule to webpack sass config
    chore: append sass rule to webpack sass config
    
    chore: append sass rule to webpack sass config

--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,32 @@
 const path = require("path")
-const flow = require("lodash.flow")
-const { override, addWebpackModuleRule } = require("customize-cra")
+const { override } = require("customize-cra")
+
+const addWebpackModuleRule = (rule) => (config) => {
+  for (let _rule of config.module.rules) {
+    if (_rule.oneOf) {
+      for (let testItem of _rule.oneOf) {
+        if (testItem.test.toString() === rule.test.toString()) {
+          delete rule.test
+          testItem.use.push(rule)
+          return config
+        }
+      }
+      _rule.oneOf.unshift(rule)
+      return config
+    }
+  }
+  return config
+}
 
 module.exports = {
-  webpack: flow(
-    override(
-      addWebpackModuleRule({
-        test: /\.(scss|sass)$/,
-        loader: "sass-resources-loader",
-        options: {
-          resources: "./src/global.scss",
-        },
-      })
-    ),
+  webpack: override(
+    addWebpackModuleRule({
+      test: /\.(scss|sass)$/,
+      loader: "sass-resources-loader",
+      options: {
+        resources: "./src/global.scss",
+      },
+    }),
     (config, env) => {
       config.resolve.modules.push(path.resolve(__dirname, "src"))
       return config
